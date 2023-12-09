a962b79d705dc99acb700db8d557f2f804aee682
Raul <raull.gasanovv@gmail.com>
2023-12-09 03:25:57 +0300

    add stylelint, fix all styles

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,73 +1,65 @@
-import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import {BuildOptions} from "./types/config";
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+   const svgLoader = {
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+   };
 
-    const svgLoader = {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-    }
+   const babelLoader = {
+      test: /\.(js|jsx|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+         loader: 'babel-loader',
+         options: {
+            presets: ['@babel/preset-env'],
+         },
+      },
+   };
 
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
+   const cssLoaders = {
+      test: /\.s[ac]ss$/i,
+      use: [
+      // Creates `style` nodes from JS strings
+         isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+         // Translates CSS into CommonJS
+         {
+            loader: 'css-loader',
             options: {
-                presets: ['@babel/preset-env'],
-                plugins:
-                    [
-                        "i18next-extract",
-                        {
-                            locales: ["ru", "en"]
-                        }
-                    ]
-            }
-        }
-    }
-
-    const cssLoaders = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            // Creates `style` nodes from JS strings
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            {
-                loader: "css-loader",
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
-                    }
-                }
+               modules: {
+                  auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                  localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]',
+               },
             },
-            // Compiles Sass to CSS
-            "sass-loader",
-        ],
-    }
+         },
+         // Compiles Sass to CSS
+         'sass-loader',
+      ],
+   };
 
-    // Если не используем typescript - нужен babel-loader
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+   // Если не используем typescript - нужен babel-loader
+   const typescriptLoader = {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+   };
 
-    const fileLoader = {
-        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
-        use: [
-            {
-                loader: 'file-loader',
-            },
-        ],
-    }
+   const fileLoader = {
+      test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+      use: [
+         {
+            loader: 'file-loader',
+         },
+      ],
+   };
 
-    return [
-        fileLoader,
-        svgLoader,
-        babelLoader,
-        typescriptLoader,
-        cssLoaders
-    ]
+   return [
+      fileLoader,
+      svgLoader,
+      babelLoader,
+      typescriptLoader,
+      cssLoaders,
+   ];
 }
