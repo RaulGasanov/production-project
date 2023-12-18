49cf02ed8ffe256bf48be5050de6b173e4912ec6
Raul <raull.gasanovv@gmail.com>
2023-12-19 02:44:14 +0300

    add react testing library, add tests for Button, Sidebar

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,32 +1,40 @@
-import { classNames } from './classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 describe('classNames', () => {
-   test('with only first param,', () => {
-      expect(classNames('some')).toBe('some');
+   test('with only first param', () => {
+      expect(classNames('someClass')).toBe('someClass');
    });
 
    test('with additional class', () => {
-      expect(classNames('some', {}, ['class1', 'class2']))
-         .toBe('some class1 class2');
+      const expected = 'someClass class1 class2';
+      expect(classNames('someClass', {}, ['class1', 'class2']))
+         .toBe(expected);
    });
 
    test('with mods', () => {
-      const expected = 'some class1 class2 hovered';
+      const expected = 'someClass class1 class2 hovered scrollable';
       expect(classNames(
-         'some',
+         'someClass',
+         { hovered: true, scrollable: true },
+         ['class1', 'class2'],
+      )).toBe(expected);
+   });
+
+   test('with mods false', () => {
+      const expected = 'someClass class1 class2 hovered';
+      expect(classNames(
+         'someClass',
          { hovered: true, scrollable: false },
          ['class1', 'class2'],
-      ))
-         .toBe(expected);
+      )).toBe(expected);
    });
 
    test('with mods undefined', () => {
-      const expected = 'some class1 class2 hovered';
+      const expected = 'someClass class1 class2 hovered';
       expect(classNames(
-         'some',
+         'someClass',
          { hovered: true, scrollable: undefined },
          ['class1', 'class2'],
-      ))
-         .toBe(expected);
+      )).toBe(expected);
    });
 });
